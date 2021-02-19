c8768bd7168b43809cbebd3e4deb2ec198a512ac
Charles Hache MSI <OzHache@gmail.com>
2021-02-18 18:32:50 -0800

    Health Sliders
    Enemies now have Health Sliders

---
+++ Mayor NPC/Assets/HealthMonitor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthMonitor : MonoBehaviour
+{
+    //get the Combatant on this object
+    [SerializeField]private Combatant combatant;
+    //slider
+    [SerializeField]private Slider slider;
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.maxValue = combatant.getMaxHealth;
+        slider.value = combatant.healthRemaining;
+    }
+}
